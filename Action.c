86ce1556ecf74d0115215b40feba1a789c0129c5
unknown <JAYAN10@IKEA>
2019-03-29 16:33:57 +0100

    This is my first script

---
+++ Action.c
@@ -0,0 +1,24 @@
+Action()
+{
+	
+	web_url("justcheck",
+
+    "URL=https://ppeirw101.ikeadt.com/se/sv/catalog/products/70116931/",
+
+    "TargetFrame=",
+
+    "TargetBrowser=Mercury Technologies",
+
+    "Resource=0",
+
+    "RecContentType=text/html",
+
+    "Snapshot=t1.inf",
+
+    "Mode=HTML",
+
+    LAST );
+	
+	
+	return 0;
+}
